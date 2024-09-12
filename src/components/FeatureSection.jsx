fd09a7d27aa8195d0d5c25297fd9dee4e5e73986
MrVisionaryGenius <mohammedsaadparbulkar@gmail.com>
2024-09-12 14:50:56 +0530

    second commit

---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,50 @@
+import { features } from "../constants";
+
+const FeatureSection = () => {
+  return (
+    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
+      <div className="text-center">
+        <span className="bg-[#004AAD] text-[#101720] rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+          Feature
+        </span>
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide relative">
+          <span className="relative z-10 text-[#004AAD]">Key Benefits </span>
+          <span className="bg-gradient-to-r from-[#004AAD] to-[#004AAD] text-transparent bg-clip-text relative z-10">
+            For Recruiters
+          </span>
+          <span
+            className="absolute inset-0 text-black z-0"
+            style={{
+              textShadow:
+                "2px 2px 0 #000, -2px 2px 0 #000, 2px -2px 0 #000, -2px -2px 0 #000",
+            }}
+            aria-hidden="true"
+          >
+            Key Benefits For Recruiters
+          </span>
+        </h2>
+      </div>
+      <div className="flex flex-wrap mt-10 lg:mt-20">
+        {features.map((feature, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+            <div className="flex">
+              <div className="flex mx-6 h-10 w-10 p-2 bg-[#004AAD] text-[#101720] justify-center items-center rounded-full">
+                {feature.icon}
+              </div>
+              <div>
+                <h5 className="mt-1 mb-6 text-xl text-[#101720]">
+                  {feature.text}
+                </h5>
+                <p className="text-md p-2 mb-20 text-neutral-500">
+                  {feature.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FeatureSection;
---
+++ src/constants/index.jsx
@@ -0,0 +1,184 @@
+import { BotMessageSquare } from "lucide-react";
+import { BatteryCharging } from "lucide-react";
+import { Fingerprint } from "lucide-react";
+import { ShieldHalf } from "lucide-react";
+import { PlugZap } from "lucide-react";
+
+
+import user1 from "../assets/profile-pictures/user1.jpg";
+import user2 from "../assets/profile-pictures/user2.jpg";
+import user3 from "../assets/profile-pictures/user3.jpg";
+import user4 from "../assets/profile-pictures/user4.jpg";
+import user5 from "../assets/profile-pictures/user5.jpg";
+import user6 from "../assets/profile-pictures/user6.jpg";
+
+export const navItems = [
+  { label: "Features", href: "#" },
+  { label: "Workflow", href: "#" },
+  { label: "Pricing", href: "#" },
+  { label: "Testimonials", href: "#" },
+];
+
+export const testimonials = [
+  {
+    user: "John Doe",
+    company: "Stellar Solutions",
+    image: user1,
+    text: "I am extremely satisfied with the services provided. The team was responsive, professional, and delivered results beyond my expectations.",
+  },
+  {
+    user: "Jane Smith",
+    company: "Blue Horizon Technologies",
+    image: user2,
+    text: "I couldn't be happier with the outcome of our project. The team's creativity and problem-solving skills were instrumental in bringing our vision to life",
+  },
+  {
+    user: "David Johnson",
+    company: "Quantum Innovations",
+    image: user3,
+    text: "Working with this company was a pleasure. Their attention to detail and commitment to excellence are commendable. I would highly recommend them to anyone looking for top-notch service.",
+  },
+  {
+    user: "Ronee Brown",
+    company: "Fusion Dynamics",
+    image: user4,
+    text: "Working with the team at XYZ Company was a game-changer for our project. Their attention to detail and innovative solutions helped us achieve our goals faster than we thought possible. We are grateful for their expertise and professionalism!",
+  },
+  {
+    user: "Michael Wilson",
+    company: "Visionary Creations",
+    image: user5,
+    text: "I am amazed by the level of professionalism and dedication shown by the team. They were able to exceed our expectations and deliver outstanding results.",
+  },
+  {
+    user: "Emily Davis",
+    company: "Synergy Systems",
+    image: user6,
+    text: "The team went above and beyond to ensure our project was a success. Their expertise and dedication are unmatched. I look forward to working with them again in the future.",
+  },
+];
+
+export const features = [
+  {
+    icon: <BotMessageSquare />,
+    text: "Save Hours Within the Day",
+    description:
+      "Drastically reduce the time spent on searching for suitable schools.",
+  },
+  {
+    icon: <Fingerprint />,
+    text: "Know Exactly Where Your Teachers Can Get To, in Seconds",
+    description:
+      "Immediate results help in making quick decisions.",
+  },
+  {
+    icon: <ShieldHalf />,
+    text: "Decrease Time from Initial Candidate Call to Placement",
+    description:
+      "Accelerate the placement process, enhancing efficiency.",
+  },
+  {
+    icon: <BatteryCharging />,
+    text: "Make More Placements, Make More Money",
+    description:
+      "Increased efficiency leads to more successful placements and higher revenue.",
+  },
+  {
+    icon: <PlugZap />,
+    text: "Saving Hours per Week",
+    description:
+      "opens up the opportunity for your recruiters to spend time doing what youdo best...Recruit!",
+  },
+];
+
+
+export const checklistItems = [
+  {
+    title: "Locates the Closest Schools",
+    description:
+      "Locates the closest schools that your candidates can get to in seconds.",
+  },
+  {
+    title: "Choose Commute Preferences",
+    description:
+      "Choose how your candidates would prefer to commute, as well as their max commute time.",
+  },
+  {
+    title: "Google Maps Filtering",
+    description:
+      "Filters the schools using Google Maps technology.",
+  },
+  {
+    title: "Ideal for Temp Staff Recruiting",
+    description:
+      "Perfect for recruiting temp staff, where time is of the essence.",
+  },
+  {
+    title: "Grow Your Business",
+    description:
+      "The perfect tool to help you build your business.",
+  },
+  {
+    title: "Market Candidates to Relevant Schools",
+    description:
+      "Market your candidates out to all the relevant schools.",
+  },
+];
+
+
+export const pricingOptions = [
+  {
+    title: "Free",
+    price: "$0",
+    features: [
+      "Private board sharing",
+      "5 Gb Storage",
+      "Web Analytics",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Pro",
+    price: "$10",
+    features: [
+      "Private board sharing",
+      "10 Gb Storage",
+      "Web Analytics (Advance)",
+      "Private Mode",
+    ],
+  },
+  {
+    title: "Enterprise",
+    price: "$200",
+    features: [
+      "Private board sharing",
+      "Unlimited Storage",
+      "High Performance Network",
+      "Private Mode",
+    ],
+  },
+];
+
+export const resourcesLinks = [
+  { href: "#", text: "Getting Started" },
+  { href: "#", text: "Documentation" },
+  { href: "#", text: "Tutorials" },
+  { href: "#", text: "API Reference" },
+  { href: "#", text: "Community Forums" },
+];
+
+export const platformLinks = [
+  { href: "#", text: "Features" },
+  { href: "#", text: "Supported Devices" },
+  { href: "#", text: "System Requirements" },
+  { href: "#", text: "Downloads" },
+  { href: "#", text: "Release Notes" },
+];
+
+export const communityLinks = [
+  { href: "#", text: "Events" },
+  { href: "#", text: "Meetups" },
+  { href: "#", text: "Conferences" },
+  { href: "#", text: "Hackathons" },
+  { href: "#", text: "Jobs" },
+];
